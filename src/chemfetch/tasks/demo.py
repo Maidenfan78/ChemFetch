79b9a8351252ad57dc85138c41e86d296cb17891
Maidenfan78 <36154326+Maidenfan78@users.noreply.github.com>
2025-07-20 17:09:03 +1000

    chore: silence mypy missing imports

--- src/chemfetch/tasks/demo.py
+++ src/chemfetch/tasks/demo.py
@@ -2,6 +2,7 @@
 from .app import celery_app
 
 
-@celery_app.task(name="chemfetch.add")  # name is optional but nice
+@celery_app.task(name="chemfetch.add")  # type: ignore[misc]
 def add(x: int, y: int) -> int:
+    """Add two numbers asynchronously."""
     return x + y
